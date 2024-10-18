d9bc22dde9e08b8fafb5ba67ba89ab8a7d74911f
Joao Felipe Silveira <joaofe747@gmail.com>
2024-10-18 14:56:01 -0400

    first commit - server

---
+++ Notes.md
@@ -0,0 +1,11 @@
+## Apollo Client
+
+Apollo Client is a comprehensive state management library for JavaScript that enables you to manage both local and remote data with GraphQL. Use it to fetch, cache, and modify application data, all while automatically updating your UI.
+
+## What are TypeDefs? (or Type Definitions)
+
+Type definitions define the shape of the data available in the GraphQL API. They specify the types of objects that can be queried and the relationships between them.
+
+## What are Resolvers?
+
+Resolvers are functions that determine how to fetch the data associated with each field in the schema.
---
+++ backend/typeDefs/user.typeDef.js
@@ -0,0 +1,40 @@
+const userTypeDef = `#graphql
+  type User {
+    _id: ID!
+    username: String!
+    password: String!
+    profilePicture: String
+    gender: String!
+  }
+
+# tell my schema what type of queries I can have for my users
+  type Query {
+    users: [User!]
+    authUser: User
+    user(userId:ID!): User
+  }
+
+  type Mutation {
+    signUp(input: signUpInput!): User
+    login(input: LoginInput!): User
+    logout: LogOutResponse
+  }
+
+  input signUpInput {
+    username: String!
+    name: String!
+    password: String!
+    gender: String!
+  }
+
+  input LoginInput {
+    username: String!
+    password: String!
+  }
+
+  type LogOutResponse {
+    message: String!
+  }
+`
+
+export default userTypeDef;
