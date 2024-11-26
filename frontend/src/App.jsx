11441673b6323620e5aa90c2f0749876728503a6
Joao Felipe Silveira <joaofe747@gmail.com>
2024-11-26 14:52:14 -0500

    setting up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import SignUpPage from "./pages/SignUpPage"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-red-500">Hello</h1>
+      <Routes>
+        {/* <Route path='/' element={<HomePage />} /> */}
+				{/* <Route path='/login' element={<LoginPage />} /> */}
+				<Route path='/signup' element={<SignUpPage />} />
+				{/* <Route path='/transaction/:id' element={<TransactionPage />} /> */}
+				{/* <Route path='*' element={<NotFound />} /> */}
+      </Routes>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import GridBackground from './components/ui/GridBackground.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <GridBackground>
+        <App />
+      </GridBackground>
+    </BrowserRouter>
   </StrictMode>,
 )
